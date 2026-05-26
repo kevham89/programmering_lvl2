37c1454665ce6e7319ceb1bea26af239ab31a144
kevham89 <kevin.hammarstedt@gmail.com>
2026-05-26 15:03:26 +0200

    saker

--- lektion-02ö1.py
+++ lektion-02ö1.py
@@ -1,7 +1,24 @@
 from mina_funktioner import namn, pnamn, pnummer, save_file, read_file
-for x in range(5):
-    input_namn = namn()
-    pnamn(input_namn)
-    input_nummer = pnummer()
-    save_file(input_namn, input_nummer)
-read_file()
+while True:
+    print("1. Ange data")
+    print("2. Spara data")
+    print("3. Läs data")
+    print("4. Stoppa programmet")
+    val = input("Mata in ditt val: ")
+    try:
+        val = int(val)
+    except ValueError:
+        print("Ange en siffra!")
+        continue        
+    if val == 1:
+        input_namn = namn()
+        pnamn(input_namn)
+        input_nummer = pnummer()
+    elif val == 2:
+        save_file(input_namn, input_nummer)
+    elif val == 3:
+        read_file()
+    elif val == 4:
+        break
+    else:
+        print("fel val")
--- mina_funktioner/utills.py
+++ mina_funktioner/utills.py
@@ -3,10 +3,15 @@ def namn():
 def pnamn(input_namn):
     print(f"Hejsan {input_namn.title()}!")
 def pnummer():
-    return int(input("Vad har du för nummer? "))
+    while True:
+        try:
+            return int(input("Vad har du för nummer? "))
+        except ValueError:
+            print("Ogiligt nummer, försök igen!")
 def save_file(pnamn, pnummer):
     with open("save.txt", "a") as fil:
         fil.write(pnamn + "," + str(pnummer) + "\n")
+        print("Data sparad!")
 def read_file():
     with open("save.txt", "r") as fil:
         MyContent = fil.read()
